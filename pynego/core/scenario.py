5f27fac6a56f405fb3f0e0d7d3f07ad3b047192f
ovllee3 <uestcliming@gmail.com>
2019-04-26 17:51:01 +0800

    define issue type

--- pynego/core/scenario.py
+++ pynego/core/scenario.py
@@ -3,3 +3,14 @@ Include two ways:
 1. Load scenario from the json file
 2. Define the scenario via code.
 '''
+
+import issue
+
+def getScenarioviaJson(jsonfile):
+    '''create the scenario class via json file
+    '''
+    pass
+
+
+class Scenario(object):
+    pass
--- pynego/core/utility.py
+++ pynego/core/utility.py
@@ -1,7 +1,15 @@
 '''get the bids utility of the agent
 '''
 
-def getUtilityViaJson():
+
+def evaluateUtility(Bid):
+    pass
+
+
+def getUtilitySpaceViaJson(file):
     '''get the utility from the json file'''
     pass
 
+
+class UtilitySpace(object):
+    pass
